4a366dc3094a474fe8f55cbb572848c4ef642458
mathiasnordberg <96matnor@BK-S2176.local.boden.se>
2014-10-15 14:55:48 +0200

    uppdaterad

--- Abstractclass.java
+++ Abstractclass.java
@@ -1,4 +1,24 @@
-public Abstractclass{
+public abstract AbstractClass{
 
+	static int nbrOfHorses;
+	private int startNbr;
+	private SimplerWindow win;
+	private Point p;
+	private int speed;
+	private int nbrOfSteps;
+	private int direction; //lagrar riktning som 1, 2, 3, 4.
 
-{
+	AbstractHorse(int x, int y, SimplerWindow win, int startNbr){
+	abstract void forward()
+	abstract void turnLeft()
+	abstract void turnRight()
+	abstract turnNorth()
+	abstract void speedUp()
+	abstract void speedDown()
+	public double getX()
+	public double getY()
+	public int getstartNbr;
+	public int getNbrOfSteps()
+    }
+}
+
